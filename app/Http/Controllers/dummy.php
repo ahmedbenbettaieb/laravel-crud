405fe40eb91f4a19de63b79dc57730d3a45837cf
Ahmed Ben Bettaieb <58952201+ahmedbenbettaieb@users.noreply.github.com>
2023-09-20 16:53:45 +0100

    first commit

---
+++ app/Http/Controllers/dummy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dummy extends Controller
+{
+    function getData()
+    {
+        return ["name" => "anil"];
+    }
+}
